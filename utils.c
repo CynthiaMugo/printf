1c4d3a53d98757bac0d1a8b50c9113551cdeeb0d
CynthiaMugo <cynthiamugo25@gmail.com>
2022-08-08 13:01:35 -0700

    utils fix

--- utils.c
+++ utils.c
@@ -28,6 +28,9 @@ int append_hexa_code(char ascii_code, char buffer[], int i)
 	if (ascii_code < 0)
 		ascii_code *= -1;
 
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
 	buffer[i++] = map_to[ascii_code / 16];
 	buffer[i] = map_to[ascii_code % 16];
 
