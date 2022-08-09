402c1f805bd98652d895e6767b3aafd868c68ba6
tilafu <tilafumusic@gmail.com>
2022-08-09 17:00:50 +0000

    main.c

--- main.h
+++ main.h
@@ -42,6 +42,7 @@ int handle_print(const char *fmt, int *i, va_list list, char buffer[],
 
 /************************* FUNCTIONS *************************/
 /* functions to print chars and strings */
+int main(void);
 int print_char(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
